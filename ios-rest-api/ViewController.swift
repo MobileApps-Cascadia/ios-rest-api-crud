7059c32b8713545bf3d7d27add37a8df2620d97b
Andrea Harrison <Andrealharrison@gmail.com>
2022-10-13 15:55:33 -0700

    mosty complete

--- ios-rest-api/ViewController.swift
+++ ios-rest-api/ViewController.swift
@@ -18,42 +18,67 @@ class User: Codable {
     var SID: String?
     
     // Read an User record from the server
-    static func fetch(withID id:Int){
-            let URLstring = DomainURL + "users/\(id)"
-            if let url = URL.init(string: URLstring){
-                let task = URLSession.shared.dataTask(with: url, completionHandler:
-                {(dataFromAPI, response, error) in
-                    print(String.init(data:dataFromAPI!, encoding: .ascii) ?? "no data")
-                    if let myUser = try? JSONDecoder().decode(User.self, from:  dataFromAPI!){
-                        print(myUser.FirstName ?? "No name")
-                    }
-                })
-                task.resume()
-            }
+    static func fetch(withID id:Int, completionHandler: @escaping (User)->Void){
+        let URLstring = DomainURL + "users/\(id)"
+        if let url = URL.init(string: URLstring){
+            let task = URLSession.shared.dataTask(with: url, completionHandler:
+                                                    {(dataFromAPI, response, error) in
+                print(String.init(data:dataFromAPI!, encoding: .ascii) ?? "no data")
+                if let myUser = try? JSONDecoder().decode(User.self, from:  dataFromAPI!){
+                    print(myUser.FirstName ?? "No name")
+                    completionHandler(myUser)
+                }
+            })
+            task.resume()
+        }
+        
     }
     // Create a new User record using a REST API "POST"
     func postToServer(){
         let URLstring = DomainURL + "users/"
-        var postRequest = URLRequest.init(url: URL.init(string: URLstring)!)
-        postRequest.httpMethod = "POST"
         
-        //TODO: Encode the user object itself as JSON and assign to the body
+        var req = URLRequest.init(url: URL.init(string: URLstring)!)
+        req.httpMethod = "POST"
         
-        //TODO: Create the URLSession task to invoke the request
+        req.httpBody = try? JSONEncoder().encode(self)
         
-        //task.resume()
+        let task = URLSession.shared.dataTask(with: req) { (data, response, error) in print (String.init(data: data!, encoding: .ascii) ?? "no data")
+        }
+        task.resume()
+        
     }
     
-    // Update this User record using a REST API "PUT"
+
+    
     func updateServer(withID id:Int){
+        guard self.SID != nil else {return}
+        let URLstring = DomainURL + "users/id/\(self.SID!)"
+        var req = URLRequest.init(url: URL.init(string: URLstring)!)
+        req.httpMethod = "PUT"
         
+        req.httpBody = try? JSONEncoder().encode(self)
+        
+        let task = URLSession.shared.dataTask(with: req) { (data, response, error) in print (String.init(data: data!, encoding: .ascii) ?? "no data")
+        }
+        task.resume()
     }
+    
+
+     
     
     // Delete this User record using a REST API "DELETE"
     func deleteFromServer(withID id:Int){
+        guard self.SID != nil else {return}
+        let URLstring = DomainURL + "users/id/\(self.SID!)"
+        var req = URLRequest.init(url: URL.init(string: URLstring)!)
+        req.httpMethod = "DELETE"
+        
+        let task = URLSession.shared.dataTask(with: req) { (data, response, error) in print (String.init(data: data!, encoding: .ascii) ?? "no data")
+        }
+        task.resume()
         
     }
-}
+
 
 
 
@@ -65,10 +90,18 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        User.fetch(withID: 2)
+        User.fetch(withID: 2) { (myUser) in
+            print (myUser.FirstName ?? "no name")
+            myUser.FirstName = "new name"
+            myUser.deleteFromServer(withID: 2)
+           //myUser.updateServer()
+            //if let myUser = try? JSONEncoder().encode(myUser) {
+               // print (myUser)
+            }
+        }
         
         //TODO: Assign values to this User object properties
-        let myUser = User()
+    let myUser = User()
         myUser.FirstName = nil
         myUser.LastName = nil
         myUser.PhoneNumber = nil
@@ -76,12 +109,12 @@ class ViewController: UIViewController {
         //Test POST method
         myUser.postToServer()
         
-        //Test PUT method
+        //Test PUT method2
         myUser.SID = "123456789"
-        //myUser.updateServer(withID: <#T##Int#>)
+        myUser.updateServer(withID: 123456789)
         
         //Test DELETE method
-        //myUser.deleteFromServer(withID: <#T##Int#>)
+        myUser.deleteFromServer(withID: 123456789)
         
     }
 
