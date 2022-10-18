e40b964473f861d7839afdc5cbe19cb50f059347
Shahin Nezam <shahinnezam@hotmail.com>
2022-10-18 11:40:51 -0700

    Completed and verified via postman

--- ios-rest-api/ViewController.swift
+++ ios-rest-api/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-    let DomainURL = "http://216.186.69.45/services/device/"
+let DomainURL = "https://mikethetall.pythonanywhere.com/"
     
 class User: Codable {
     var UserID: String?
@@ -19,7 +19,7 @@ class User: Codable {
     
     // Read an User record from the server
     static func fetch(withID id:Int){
-            let URLstring = DomainURL + "users/\(id)"
+            let URLstring = DomainURL + "users/\(String(id))"
             if let url = URL.init(string: URLstring){
                 let task = URLSession.shared.dataTask(with: url, completionHandler:
                 {(dataFromAPI, response, error) in
@@ -33,15 +33,19 @@ class User: Codable {
     }
     // Create a new User record using a REST API "POST"
     func postToServer(){
-        let URLstring = DomainURL + "users/"
+        let URLstring = DomainURL + "users"
         var postRequest = URLRequest.init(url: URL.init(string: URLstring)!)
         postRequest.httpMethod = "POST"
         
         //TODO: Encode the user object itself as JSON and assign to the body
-        
+        postRequest.httpBody = try? JSONEncoder().encode(self)
+        postRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
         //TODO: Create the URLSession task to invoke the request
-        
-        //task.resume()
+        let task = URLSession.shared.dataTask(with: postRequest) {
+            (data, response, error) in
+                print(String.init(data: data!, encoding: .ascii) ?? "no data")
+        }
+        task.resume()
     }
     
     // Update this User record using a REST API "PUT"
@@ -51,6 +55,15 @@ class User: Codable {
     
     // Delete this User record using a REST API "DELETE"
     func deleteFromServer(withID id:Int){
+        let URLstring = DomainURL + "users/\(String(id))"
+               var postRequest = URLRequest.init(url: URL.init(string: URLstring)!)
+               postRequest.httpMethod = "DELETE"
+
+               let task = URLSession.shared.dataTask(with: postRequest) {
+                   (data, response, error) in
+                    print(String.init(data: data!, encoding: .ascii) ?? "no data")
+               }
+               task.resume()
         
     }
 }
@@ -65,23 +78,23 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        User.fetch(withID: 2)
+        User.fetch(withID: 1)
         
         //TODO: Assign values to this User object properties
         let myUser = User()
-        myUser.FirstName = nil
-        myUser.LastName = nil
-        myUser.PhoneNumber = nil
+        myUser.FirstName = "Ali"
+        myUser.LastName = "Ababwa"
+        myUser.PhoneNumber = "2062062206"
         
         //Test POST method
-        myUser.postToServer()
+        //myUser.postToServer()
         
         //Test PUT method
-        myUser.SID = "123456789"
+        //myUser.SID = "123456789"
         //myUser.updateServer(withID: <#T##Int#>)
         
         //Test DELETE method
-        //myUser.deleteFromServer(withID: <#T##Int#>)
+        myUser.deleteFromServer(withID: 5)
         
     }
 
